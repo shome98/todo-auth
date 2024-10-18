14e1d41a1a35460d7cf0673830c5d1244d184b38
Sowparna Shome <soupornoshome@gmail.com>
2024-10-18 23:21:12 +0530

    slight change

--- todo-auth-api/db/dbConnect.ts
+++ todo-auth-api/db/dbConnect.ts
@@ -4,13 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const dbConnect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI as string);
-    console.log('MongoDB connected!!!');
-  } catch (error) {
-    console.error('MongoDB connection failed!!! ', error);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(process.env.MONGODB_URI as string);
+        console.log('MongoDB connected!!!');
+    } catch (error) {
+        console.error('MongoDB connection failed!!! ', error);
+    }
 };
 
 export default dbConnect;
