47fd266bcc62b6063559a54fa8763f7e901e19eb
Sowparna Shome <soupornoshome@gmail.com>
2024-10-18 22:49:25 +0530

    create registration

---
+++ todo-auth-api/controllers/user.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import User from "../models/user.model";
+
+//User registration
+export const register = async (req: Request, res: Response) => {
+    const { username, email, password } = req.body;
+    if (username || password === null) {
+        res.end("Please the provide the full information");
+    }
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = await User.create({ username, email, password: hashedPassword });
+        res.status(201).json({ message: 'User registered successfully', userId: newUser._id });
+    } catch (error) {
+        res.status(400).json({ message: 'Error registering user', error });
+    }
+}
