3994f5008199a3eb8d408be3fbc090ece3e60279
Sowparna Shome <soupornoshome@gmail.com>
2024-10-20 23:15:28 +0530

    add refresh token field

--- todo-auth-api/models/user.model.ts
+++ todo-auth-api/models/user.model.ts
@@ -4,11 +4,13 @@ export interface IUser extends Document {
   username: string;
   email: string;
   password: string;
+  refreshToken:string
 }
 const userSchema = new Schema<IUser>({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    refreshToken:{type:String}
 }, { timestamps: true });
 const User = mongoose.model<IUser>("User", userSchema);
 export default User;
