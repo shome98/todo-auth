5267b8c29a3c925b005b1b38c5d39d60b55daee8
Sowparna Shome <soupornoshome@gmail.com>
2024-10-20 19:51:39 +0530

    create an api error

---
+++ todo-auth-api/utils/ApiError.ts
@@ -0,0 +1,26 @@
+export class ApiError extends Error {
+    statusCode: number;
+    data: any;
+    success: boolean;
+    errors: string[];
+
+    constructor(
+        statusCode: number,
+        message = "Something bad happened",
+        errors: string[] = [],
+        stack = ""
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.data = null;
+        this.message = message;
+        this.success = false;
+        this.errors = errors;
+
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
