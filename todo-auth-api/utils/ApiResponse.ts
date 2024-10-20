82fce60b09a4402672cab2e6488ee90b4cc95291
Sowparna Shome <soupornoshome@gmail.com>
2024-10-20 20:06:15 +0530

    create api response

---
+++ todo-auth-api/utils/ApiResponse.ts
@@ -0,0 +1,13 @@
+export class ApiResponse<T> {
+    statusCode: number;
+    data: T;
+    message: string;
+    success: boolean;
+
+    constructor(statusCode: number, data: T, message = "Successful") {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = statusCode < 400;
+    }
+}
