c5d61fec709fbf9b9083ae4947a2130948607e99
Sowparna Shome <soupornoshome@gmail.com>
2024-10-26 00:14:09 +0530

    add interface for todo

--- todo-auth-client/src/components/Check.tsx
+++ todo-auth-client/src/components/Check.tsx
@@ -3,10 +3,17 @@ import {  useState } from "react";
 import { getTodos } from "../services/todoService";
 import { loginUser, logoutUser } from "../services/authService";
 
+interface Todo {
+    _id: number; 
+    title: string;
+    description: string;
+    completed: boolean;
+}
+
 function Check() {
     const [login, setLogin] = useState(false);
     //const [token, setToken] = useState('');
-    const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useState<Todo[]>([]);
     const [got, setGot] = useState(false);
     const handleLogin = async () => {
         const res = await loginUser({ "username": "username eight", "password": "password" });
@@ -41,7 +48,7 @@ function Check() {
         }
         console.log(todos);
         console.log(todos.length);
-        todos.map(todo => console.log(todo.title));
+        todos.map(todo => console.log(`${todo._id}___${todo.title}___${todo.description}___${todo.completed}`));
         
     }
     return <><h1>hi there</h1>
@@ -49,7 +56,6 @@ function Check() {
         {!login ? <button onClick={handleLogin}>Login here</button> : <button onClick={() => handleLogout()}>logout</button>}
         <button onClick={handleGetTodos}>get all todos</button>
         <h1>{got}</h1>
-            
     </>;
 }
 export default Check;
