d17dc30feed4c7d91ac89f13b71d6120137f1eef
Sowparna Shome <soupornoshome@gmail.com>
2024-10-25 21:44:15 +0530

    remove check component

--- todo-auth-client/src/App.tsx
+++ todo-auth-client/src/App.tsx
@@ -1,10 +1,7 @@
-import Check from "./components/Check"
-
 function App() {
 
   return (
     <>
-      <Check></Check>
       <h1 className="text-3xl font-bold bg-slate-400">my todo app</h1>
     </>
   )
--- todo-auth-client/src/components/Check.tsx
+++ todo-auth-client/src/components/Check.tsx
@@ -1,14 +1,17 @@
 import {  useState } from "react";
-import { loginUser, logoutUser } from "../services/authService";
+import { loginUser, logoutUser } from "../services/authService-test";
+import { getTodos } from "../services/todoService";
 
 function Check() {
     const [login, setLogin] = useState(false);
-    const [token, setToken] = useState('');
+    //const [token, setToken] = useState('');
+    const [todos, setTodos] = useState([]);
+    const [got, setGot] = useState(false);
     const handleLogin = async () => {
-        const res = await loginUser({ "username": "username seven", "password": "password" });
+        const res = await loginUser({ "username": "username eight", "password": "password" });
         console.log(res?.data);
-        setToken(res?.data.data.accessToken);
-        console.log(token);
+        //setToken(res?.data.data.accessToken);
+        //console.log(token);
         if (res?.status === 200) {
             setLogin(true);
         }
@@ -27,9 +30,24 @@ function Check() {
             setLogin(false);
         }
     }
+
+    const handleGetTodos =  async() => {
+        const res = await getTodos();
+        console.log(res?.data);
+        if (res?.status === 200) {
+            setTodos(res.data.data);
+            setGot(true);
+        }
+        console.log(todos);
+        console.log(todos.length);
+        todos.map(todo => console.log(todo.title));
+        
+    }
     return <><h1>hi there</h1>
         <h1>{login}</h1>
-        {!login?<button onClick={handleLogin}>Login here</button>:<button onClick={()=>handleLogout()}>logout</button>}
+        {!login ? <button onClick={handleLogin}>Login here</button> : <button onClick={() => handleLogout()}>logout</button>}
+        <button onClick={handleGetTodos}>get all todos</button>
+        <h1>{got}</h1>
             
     </>;
 }
