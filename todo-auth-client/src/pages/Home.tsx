28af46e81fec6d3c4e2dfa3a66fd5e539fe0ea16
Sowparna Shome <soupornoshome@gmail.com>
2024-10-25 23:02:36 +0530

    basic home page

---
+++ todo-auth-client/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h1 className="text-4xl">Welcome to Todo App</h1>
+        </div>
+    );
+};
+
+export default Home;
