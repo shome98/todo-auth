af756e6a6d8386daf7e21ae877a860c8487e3532
Sowparna Shome <soupornoshome@gmail.com>
2024-10-25 21:43:52 +0530

    auth service async calls to login logout register update forgot password

--- todo-auth-client/src/services/authService.ts
+++ todo-auth-client/src/services/authService.ts
@@ -1,46 +1,48 @@
 import axios from "axios";
 
 const API_URL = "https://todo-auth-kappa.vercel.app/api/v1/users";
+
 export const loginUser = async (userData: { username: string; password: string }) => {
-    // const response = await fetch(`${API_URL}/login`, {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify(userData),
-    // });
     try {
-        const response = await axios.post(`${API_URL}/login`, userData,{ withCredentials: true });
+        const response = await axios.post(`${API_URL}/login`, userData, { withCredentials: true });
         return response;
     } catch (error) {
-        console.error("User can't login!!! ", error);
+        console.error("User can't login!!!", error);
     }
 };
 
 export const registerUser = async (userData: { username: string; email: string; password: string }) => {
     try {
-        const response = await axios.post(`${API_URL}/register`, userData,{ withCredentials: true });
+        const response = await axios.post(`${API_URL}/register`, userData, { withCredentials: true });
         return response;
     } catch (error) {
-        console.error("User can't register!!! ", error);
+        console.error("User can't register!!!", error);
     }
 };
 
-// export const logoutUser = async (token:string) => {
-//     try {
-//         const response = await axios.post(`${API_URL}/logout`, {},{ withCredentials: true,headers:{
-//                     Authorization: `Bearer ${token}`
-//                 } });
-//         return response;
-//     } catch (error) {
-//         console.error("User can't logout error!!! ", error);
-//     }
-// }
 export const logoutUser = async () => {
     try {
-        const response = await axios.post(`${API_URL}/logout`, {},{ withCredentials: true});
+        const response = await axios.post(`${API_URL}/logout`, {}, { withCredentials: true });
         return response;
     } catch (error) {
-        console.error("User can't logout error!!! ", error);
+        console.error("User can't logout error!!!", error);
     }
-}
+};
+
+export const forgotPassword = async (userData: { username: string; email: string }) => {
+    try {
+        const response = await axios.post(`${API_URL}/forgot-password`, userData);
+        return response;
+    } catch (error) {
+        console.error("Password reset request failed!!!", error);
+    }
+};
+
+export const updatePassword = async (passwordData: { oldPassword: string; newPassword: string }) => {
+    try {
+        const response = await axios.post(`${API_URL}/update-password`, passwordData, { withCredentials: true });
+        return response;
+    } catch (error) {
+        console.error("Password update failed!!!", error);
+    }
+};
