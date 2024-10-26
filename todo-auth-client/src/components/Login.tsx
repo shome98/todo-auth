405637eb341a4ef0542280d197e462fae980442d
Sowparna Shome <soupornoshome@gmail.com>
2024-10-26 23:30:05 +0530

    login

---
+++ todo-auth-client/src/components/Login.tsx
@@ -0,0 +1,61 @@
+const Login = () => {
+    return (
+        <div className="h-screen bg-gradient-to-br from-blue-600 to-cyan-300 flex justify-center items-center w-full">
+        
+        <form method='POST' action='#'>
+            <div className="bg-white px-10 py-8 rounded-xl w-screen shadow-xl max-w-sm">
+                <div className="space-y-4">
+                    <h1 className="text-center text-2xl font-semibold text-gray-600">Login</h1>
+                    <hr/>
+                <div className="flex items-center border-2 py-2 px-3 rounded-md mb-4">
+                            <svg className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 11a4 4 0 100-8 4 4 0 000 8zm0 2c-3.31 0-6 2.69-6 6v1a1 1 0 001 1h10a1 1 0 001-1v-1c0-3.31-2.69-6-6-6z" />
+                            </svg>
+                            <input 
+                                className="pl-2 outline-none border-none w-full" 
+                                type="text" 
+                                name="username" 
+                                placeholder="Username" 
+                                required 
+                            />
+                        </div>
+                <div className="flex items-center border-2 py-2 px-3 rounded-md mb-4">
+                            <svg className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+                            </svg>
+                            <input 
+                                className="pl-2 outline-none border-none w-full" 
+                                type="password" 
+                                name="password" 
+                                placeholder="Password" 
+                                required 
+                            />
+                </div>
+                </div>
+                {/*
+                <div className="flex justify-center items-center mt-4">
+                    <p className="inline-flex items-center text-gray-700 font-medium text-xs text-center">
+                        <input type="checkbox" id="rememberMeCheckbox" name="rememberMe" className="mr-2"/>
+                        <span className="text-xs font-semibold">Remember me?</span>
+                    </p>
+                </div>*/}
+
+                <button type="submit" value="login" id="login" className="mt-6 w-full shadow-xl bg-gradient-to-tr from-blue-600 to-red-400 hover:to-red-700 text-indigo-100 py-2 rounded-md text-lg tracking-wide transition duration-1000">Login</button>
+                <hr/>
+                <div className="flex justify-center items-center mt-4">
+                    <p className="inline-flex items-center text-gray-700 font-medium text-xs text-center">
+                        <span className="ml-2">You don't have an account?<a href="/register" className="text-xs ml-2 text-blue-500 font-semibold">Register now &rarr;</a>
+                        </span>
+                    </p>
+                </div>
+            </div>
+            <div className="pt-6 text-base font-semibold leading-7">
+                <p className="font-sans text-red-500 text-md hover:text-red-800">
+                <a href="/" className="absolute">&larr; Home</a>
+                </p>
+            </div>
+        </form>
+    </div>
+    )
+}
+export default Login;
