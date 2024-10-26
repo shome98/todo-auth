954f075e163a9382167272d9eb40abc0668bd77c
Sowparna Shome <soupornoshome@gmail.com>
2024-10-26 23:29:51 +0530

    routes declare for login register

--- todo-auth-client/src/App.tsx
+++ todo-auth-client/src/App.tsx
@@ -1,10 +1,18 @@
-
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import Home from "./pages/Home"
+import Login from "./components/Login"
+import Register from "./components/Register"
 function App() {
 
   return (
     <>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/> } />
+          <Route path="/login" element={<Login/> } />
+          <Route path="/register" element={<Register/> } />
+        </Routes>
+      </Router>
     </>
   )
 }
