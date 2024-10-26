a6baee0ce3ba963df47e9395ca6e8aff65a7bcb9
Sowparna Shome <soupornoshome@gmail.com>
2024-10-26 23:30:12 +0530

    register

---
+++ todo-auth-client/src/components/Register.tsx
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { register } from "../slices/authSlice";
+
+interface User{
+    username?: string;
+    email?: string;
+    password?: string;
+}
+
+const Register = () => {
+    const[user,setUser]=useState<User>()
+    // const [password, setPassword] = useState('');
+    // const [confirmPassword, setConfirmPassword] = useState('');
+    // const [error, setError] = useState('');
+    // const [username, setuserName] = useState('');
+    // const [email, setEmail] = useState('')
+    const dispatch = useDispatch();
+    //dispatch(register(user));
+    console.log(user);
+    return (
+        <div className="h-screen bg-gradient-to-br from-blue-600 to-cyan-300 flex justify-center items-center w-full">
+            <form method='POST' action='#'>
+                <div className="bg-white px-10 py-8 rounded-xl w-screen shadow-xl max-w-sm">
+                    <div className="space-y-4">
+                        <h1 className="text-center text-2xl font-semibold text-gray-600">Create your Account</h1>
+                        <hr />
+                        {/* Username Field */}
+                        <div className="flex items-center border-2 py-2 px-3 rounded-md mb-4">
+                            <svg className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 11a4 4 0 100-8 4 4 0 000 8zm0 2c-3.31 0-6 2.69-6 6v1a1 1 0 001 1h10a1 1 0 001-1v-1c0-3.31-2.69-6-6-6z" />
+                            </svg>
+                            <input 
+                                className="pl-2 outline-none border-none w-full" 
+                                type="text" 
+                                name="username" 
+                                placeholder="Username" 
+                                value={user?.username}
+                                onChange={(e)=>setUser({...user,username:e.target.value})}
+                                required 
+                            />
+                        </div>
+                        
+                        {/* Email Field */}
+                        <div className="flex items-center border-2 py-2 px-3 rounded-md mb-4">
+                            <svg className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+                            </svg>
+                            <input 
+                                className="pl-2 outline-none border-none w-full" 
+                                type="email" 
+                                name="email" 
+                                placeholder="Email" 
+                                value={user?.email}
+                                onChange={e=>setUser({...user,email:e.target.value})}
+                                required 
+                            />
+                        </div>
+
+                        {/* Password Field */}
+                        <div className="flex items-center border-2 py-2 px-3 rounded-md mb-4">
+                            <svg className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+                            </svg>
+                            <input 
+                                className="pl-2 outline-none border-none w-full" 
+                                type="password" 
+                                name="password" 
+                                placeholder="Password" 
+                                value={user?.password}
+                                onChange={e=>setUser({...user,password:e.target.value})}
+                                required 
+                            />
+                        </div>
+
+                        {/* Confirm Password Field */}
+                        <div className="flex items-center border-2 py-2 px-3 rounded-md mb-4">
+                            <svg className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+                            </svg>
+                            <input 
+                                className="pl-2 outline-none border-none w-full" 
+                                type="password" 
+                                name="confirmPassword" 
+                                placeholder="Confirm Password" 
+                                required 
+                            />
+                        </div>
+                    </div>
+                    
+                    <button type="submit" className="mt-6 w-full shadow-xl bg-gradient-to-tr from-blue-600 to-red-400 hover:to-red-700 text-indigo-100 py-2 rounded-md text-lg tracking-wide transition duration-1000">
+                        Register
+                    </button>
+                </div>
+                
+                <div className="pt-6 text-base font-semibold leading-7">
+                    <p className="font-sans text-red-500 text-md hover:text-red-800">
+                        <a href="/" className="absolute">&larr; Home</a>
+                    </p>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default Register;
