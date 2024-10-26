53b758abead4b2c1cfc7a752c6dc472ff4f09f02
Sowparna Shome <soupornoshome@gmail.com>
2024-10-27 00:14:16 +0530

    loggedin page to redirect check

--- todo-auth-client/src/App.tsx
+++ todo-auth-client/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Login from "./components/Login"
 import Register from "./components/Register"
+import LoggedInPage from "./pages/LoggedInPage"
 function App() {
 
   return (
@@ -10,7 +11,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/> } />
           <Route path="/login" element={<Login/> } />
-          <Route path="/register" element={<Register/> } />
+          <Route path="/register" element={<Register />} />
+          <Route path="/loggedin" element={<LoggedInPage />} />
         </Routes>
       </Router>
     </>
---
+++ todo-auth-client/src/pages/LoggedInPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoggedInPage = () => {
+    return (
+        <div className="h-screen bg-green-500 flex justify-center items-center">
+            <h1 className="text-4xl font-bold text-white">You're logged in!</h1>
+        </div>
+    );
+};
+
+export default LoggedInPage;
