b435a5f7f384f6a0c6c48710c85cefb020b7c4ba
shome98 <soupornoshome@gmail.com>
2024-10-30 14:36:16 +0530

    add todo modal

---
+++ todo-auth-client/src/components/todo-check/AddModal.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../store/store";
+import { addNewTodo } from "../../slices/todoSlice";
+
+// interface NewTodo{
+//     title:string;
+//     description?:string;
+// }
+interface AddModalProps{
+    onClose: () => void;
+    onSave:()=>void;
+}
+
+export const AddModal:React.FC<AddModalProps>=({onClose,onSave})=>{
+    //const [todo,setTodo]=useState<NewTodo>({title:"",description:""});
+    const [title,setTitle]=useState("");
+    const [description,setDescription]=useState("");
+    const dispatch=useDispatch<AppDispatch>();
+    const handleSave=async()=>{
+        await dispatch(addNewTodo({title,description}));
+        onSave();
+    }
+    return (<div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+    <div className="bg-white rounded-lg shadow-lg p-6 max-w-md w-full">
+      <h2 className="text-xl font-semibold mb-4">Add New Todo</h2>
+      <div className="mb-4">
+        <label className="block text-gray-700">Title</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700">Description</label>
+        <textarea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          rows={4}
+        />
+      </div>
+      <div className="flex justify-end space-x-2">
+        <button
+          className="px-4 py-2 bg-gray-200 rounded-md"
+          onClick={onClose}
+        >
+          Cancel
+        </button>
+        <button
+          className="px-4 py-2 bg-blue-500 text-white rounded-md"
+          onClick={handleSave}
+          
+        >
+          Save
+        </button>
+      </div>
+    </div>
+  </div>);
+}
--- todo-auth-client/src/components/todo-check/TodoList.tsx
+++ todo-auth-client/src/components/todo-check/TodoList.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, } from 'react';
+import React, { useEffect, useState, } from 'react';
 import TodoCard from './TodoCard';
 import { useDispatch } from 'react-redux';
 import { AppDispatch, RootState } from '../../store/store';
 import { useSelector } from 'react-redux';
 import { fetchTodos, removeExistingTodo, toggleTodoComplete } from '../../slices/todoSlice';
+import { AddModal } from './AddModal';
 
 // interface Todo {
 //   _id: string;
@@ -18,20 +19,25 @@ const TodoList: React.FC = () => {
   const todos = useSelector((state: RootState) => state.todos.todos);
   const auth = useSelector((state: RootState) => state.auth)
   const { isAuthenticated } = auth;
+  const [isAddOpen,setAddOpen]=useState(false);
+  const [isChanged,setIsChanged]=useState(0);
   useEffect(() => {
     dispatch(fetchTodos())
-  }, [dispatch,todos])
+  }, [dispatch,isChanged])
   
-  const toggleComplete = (id:string, completed:boolean) => {
-    dispatch(toggleTodoComplete({id,completed}))
+  const toggleComplete = async (id:string, completed:boolean) => {
+    await dispatch(toggleTodoComplete({id,completed}));
+    setIsChanged(isChanged+1);
   }
   const handleDeleteTodo = async (id: string) => {
     await dispatch(removeExistingTodo(id));
+    setIsChanged(isChanged+1);
   }
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
       <h1 className="text-2xl font-bold mb-6">Todo List is { isAuthenticated ?"authenticated":"not authenticated"}</h1>
+      <button onClick={()=>setAddOpen(true)} className="p-1 text-blue-500 transition-colors rounded hover:bg-red-100 focus:outline-none">Add new Todo</button>
       <div className="grid grid-cols-1 gap-6 max-w-[800px] w-full">
         {todos.length>0&&todos.map((todo) => (
           <TodoCard
@@ -46,6 +52,9 @@ const TodoList: React.FC = () => {
           />
         ))}
       </div>
+      {isAddOpen&&<AddModal
+      onClose={()=>setAddOpen(false)}
+      onSave={()=>setAddOpen(false)}/>}
     </div>
   );
 };
