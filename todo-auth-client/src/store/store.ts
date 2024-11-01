143d67fe64ca062cd78e86eed7772539c64ab2e8
Sowparna Shome <soupornoshome@gmail.com>
2024-11-02 00:22:51 +0530

    feedback store add

--- todo-auth-client/src/store/store.ts
+++ todo-auth-client/src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../slices/authSlice.ts";
 import todoReducer from "../slices/todoSlice.ts";
+import feedbackReducer from "../slices/feedbackSlice.ts"
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
         todos: todoReducer,
+        feedback:feedbackReducer
     },
 });
 export type RootState = ReturnType<typeof store.getState>;
