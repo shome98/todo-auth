5c164054da16bf5e1f4e33f276a971c288d9cc7a
Sowparna Shome <soupornoshome@gmail.com>
2024-11-03 22:48:13 +0530

    logout button

---
+++ todo-auth-client/src/components/profile/LogOutButton.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { AppDispatch } from '../../store/store'
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import { logout } from '../../slices/authSlice';
+
+const LogOutButton = () => {
+    const dispatch = useDispatch<AppDispatch>();
+    const navigate = useNavigate();
+    const handleLogout = async () => {
+         const status = await dispatch(logout());
+    if (status.meta.requestStatus === "fulfilled") {
+      toast.success("You are logged out!!!");
+      navigate('/');
+      return;
+    }
+    toast.error("Could not log you out!!!");
+    }
+  return (
+    <button onClick={handleLogout} className= "mr-10 hover:bg-red-500 p-2 rounded transition-colors text-white">
+          Logout
+        </button>
+  )
+}
+
+export default LogOutButton;
