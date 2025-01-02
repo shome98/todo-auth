20590789ad36e9acf21149b7117885d3ff51bc81
shome98 <soupornoshome@gmail.com>
2025-01-02 14:48:46 +0530

    provider add issue

--- todo-auth-nextclient/app/page.tsx
+++ todo-auth-nextclient/app/page.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 export default function Home() {
   return (
-    <><h1 className="font-bold mt-9 ml-8">Yo this is me</h1></>
+    <><Provider store={store}><h1 className="font-bold mt-9 ml-8">Yo this is me</h1></Provider></>
   );
 }
