216210439dc3225db84bd3200e74978869b00158
shome98 <soupornoshome@gmail.com>
2025-01-02 15:04:37 +0530

    wrapper wrap

--- todo-auth-nextclient/app/page.tsx
+++ todo-auth-nextclient/app/page.tsx
@@ -1,8 +1,9 @@
 import { Provider } from "react-redux";
 import store from "./store/store";
+import Wrapper from "./Wrapper";
 
 export default function Home() {
   return (
-    <><Provider store={store}><h1 className="font-bold mt-9 ml-8">Yo this is me</h1></Provider></>
+    <><Wrapper><h1 className="font-bold mt-9 ml-8">Yo this is me</h1></Wrapper></>
   );
 }
