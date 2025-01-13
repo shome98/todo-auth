930542b63b4f1ade223348bc7f2b70068bba6d35
shome98 <soupornoshome@gmail.com>
2025-01-13 16:52:11 +0530

    fix router 1

--- todo-auth-client/src/main.tsx
+++ todo-auth-client/src/main.tsx
@@ -8,10 +8,10 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
     <Provider store={store}>
+    <BrowserRouter>
       <App />
-      </Provider>
       </BrowserRouter>
+      </Provider>
   </StrictMode>,
 )
